7c6151e9138f985a7b3853ab01cf44b67301f24f
Longears00 <Melani_y@126.com>
2017-11-04 15:31:07 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'app.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.js$/, loader: 'babel-loader',  exclude: /node_modules/, query: { presets: ['env'] } },
+    ],
+  },
+};
