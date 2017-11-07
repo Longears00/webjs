db6481f6804e16312e9fdaee8d334ba8be87f5ac
Longears00 <Melani_y@126.com>
2017-11-07 15:51:46 +0800

    add reduce

---
+++ src/closures_demo.js
@@ -0,0 +1,10 @@
+
+let register = name => company => name + ' register for  ' + company;
+console.log(register('cookie')('cat world'));
+
+let dragon = [
+    { name: 'fluffykins', element: 'lightning' },
+    { name: 'noomi', element: 'lightning' },
+    { name: 'karo', element: 'fire' },
+    { name: 'doomer', element: 'timewarp' },
+];
---
+++ src/reduce_demo.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+
+let orders = [
+    { amount: 250 },
+    { amount: 400 },
+    { amount: 100 },
+    { amount: 325 },
+];
+
+let output = fs.readFileSync('./src/temp.txt', 'utf8').trim().split(' \n').map(line => line.split('\t')).reduce((customer, line)=> {
+    console.log(line[0]);
+    return customer;
+  }, {});
+// console.log(output);
+
+// var totalAmount = orders.reduce((sum, order)=> sum + order.amount, 0);
